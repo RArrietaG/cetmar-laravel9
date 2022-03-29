a3b7ae9989266059b231af0f7721ccce226ef6f3
RUTILO <rutilo.arrieta@cetmar31.edu.mx>
2022-03-29 15:55:53 -0700

    Se crea el componente para listar aspirantes

---
+++ app/Http/Livewire/Candidates/Candidates.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Candidates;
+
+use Livewire\Component;
+
+class Candidates extends Component
+{
+    public function render()
+    {
+        return view('livewire.candidates.candidates');
+    }
+}
